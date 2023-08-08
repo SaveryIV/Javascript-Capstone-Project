eb7b884c640c4d55277c6e09555c1c1d807a4a33
Uzair-Manzoor <uzair.kayani555@gmail.com>
2023-08-08 15:36:44 +0500

    add jest config file

---
+++ jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  testEnvironment: 'node', // Specifies the environment in which tests are run
+//   testEnvironment: 'jest-environment-jsdom',
+  testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'], // Specifies test file patterns
+};
