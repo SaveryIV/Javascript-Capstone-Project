6aee3701b3ce6ca2ea2936635ebb583beea5f37b
Uzair-Manzoor <uzair.kayani555@gmail.com>
2023-08-10 17:05:32 +0500

    update api file

--- src/modules/api.js
+++ src/modules/api.js
@@ -1,5 +1,13 @@
-// api.js
 const inventoryAPI = 'https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/fWAJPqcXqO3QM9dV6LYn/';
+const pokeAPI = 'https://pokeapi.co/api/v2/pokemon/';
 const likesURL = `${inventoryAPI}likes/`;
 
-export { inventoryAPI, likesURL };
+const getPokemon = async (id) => {
+  const res = await fetch(pokeAPI + id);
+  const data = await res.json();
+  return data;
+};
+
+export {
+  inventoryAPI, likesURL, pokeAPI, getPokemon,
+};
