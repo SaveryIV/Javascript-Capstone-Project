5951dbaff61ea63b80ebdd5b9124596777a1e3e1
Agustin Soria <85071975+SaveryIV@users.noreply.github.com>
2023-08-11 12:16:58 -0300

    Revert "Comment popup"

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,5 @@
 module.exports = {
-  testEnvironment: 'jest-environment-jsdom',
+  testEnvironment: 'node', // Specifies the environment in which tests are run
+  testMatch: ['**/__tests__/**/*.js?(x)', '**/?(*.)+(spec|test).js?(x)'], // Specifies test file patterns
+  //   testEnvironment: 'jest-environment-jsdom',
 };
--- src/modules/commentCounter.js
+++
@@ -1,6 +0,0 @@
-const commentCounter = () => {
-  const counter = document.querySelectorAll('.comment');
-  return counter.length;
-};
-
-export default commentCounter;
